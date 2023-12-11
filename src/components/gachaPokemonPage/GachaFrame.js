0dbfa09abe8f02067ec6137dd1dc6fdcc3cd792a
DoleH2 <levanhung27091998@gmail.com>
2023-12-11 21:52:49 +0700

    UpFromHome

--- src/components/gachaPokemonPage/GachaFrame.js
+++ src/components/gachaPokemonPage/GachaFrame.js
@@ -1,14 +1,68 @@
 import SunRay from "../utilsComponent/SunRay";
-import pokeBall from '../../img/pokeball.png';
+import pokeBall from "../../img/pokeball.png";
+import "../../scss/gachaframestyle.scss";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useEffect, useState } from "react";
+import LoadCircle from "../utilsComponent/LoadCircle";
 const GachaFrame = () => {
-    return (
-        <div className="frame-gacha-pokemon">
-            <div className="mx-auto mt-5 position-relative"
-                style={{ width: '100px', height: '100px' }}>
-                <SunRay srcImg={pokeBall} />
-            </div>
-        </div>
-    )
-}
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const [loadPokemon, setLoadPokemon] = useState(false);
+  const handleShow = () => {
+    setShow(true);
+    setLoadPokemon(false);
+  };
+  useEffect(() => {}, []);
+  return (
+    <div className="frame-gacha-pokemon">
+      <div className="frame-pokeball position-relative p-5 pt-3 bg-white rounded">
+        <p className="m-0 h4 mx-auto text-center">Click to Get Pokemon</p>
+        <img
+          src={pokeBall}
+          className="w-100 img-pokeball position-relative"
+          style={{ zIndex: "10" }}
+          onClick={handleShow}
+        ></img>
+      </div>
+      <Modal
+        className="modal-show-pokemon"
+        centered
+        show={show}
+        onHide={handleClose}
+      >
+        <Modal.Header
+          className="bg-transparent border-0"
+          closeButton
+        ></Modal.Header>
+        <Modal.Body className="d-flex justify-content-center flex-column">
+          {!loadPokemon && <LoadCircle />}
+
+          <img
+            onLoad={() => setLoadPokemon(true)}
+            className="w-100 img-pokemon"
+            style={{ zIndex: "10" }}
+            src={
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/37.png"
+            }
+          ></img>
+          {loadPokemon && (
+            <>
+              <SunRay />
+              <div className="frame-stats d-flex justify-content-between">
+                <p className="text-white">HP</p>
+                <p className="text-white">ATK</p>
+                <p className="text-white">DEF</p>
+                <p className="text-white">S.ATK</p>
+                <p className="text-white">S.DEF</p>
+                <p className="text-white">SPD</p>
+              </div>
+            </>
+          )}
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
 
 export default GachaFrame;
--- src/components/utilsComponent/SunRay.js
+++ src/components/utilsComponent/SunRay.js
@@ -1,22 +1,19 @@
-import '../../scss/sunray.scss';
+import "../../scss/sunray.scss";
 const SunRay = ({ srcImg }) => {
-    return (
-        <div className="sun bg-white">
-            <img src={srcImg} className="img-center w-100" style={{ transform: 'scale(1.5)' }}></img>
-            <div className="ray_box">
-                <div className="ray ray1"></div>
-                <div className="ray ray2"></div>
-                <div className="ray ray3"></div>
-                <div className="ray ray4"></div>
-                <div className="ray ray5"></div>
-                <div className="ray ray6"></div>
-                <div className="ray ray7"></div>
-                <div className="ray ray8"></div>
-                <div className="ray ray9"></div>
-                <div className="ray ray10"></div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="sun bg-white">
+      <div className="ray_box">
+        <div className="ray ray1"></div>
+        <div className="ray ray2"></div>
+        <div className="ray ray3"></div>
+        <div className="ray ray4"></div>
+        <div className="ray ray5"></div>
+        <div className="ray ray6"></div>
+        <div className="ray ray7"></div>
+        <div className="ray ray8"></div>
+      </div>
+    </div>
+  );
+};
 
 export default SunRay;
